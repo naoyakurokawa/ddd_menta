6045f2984d1676b0009dec061db5d0acc3e1a7d0
kurokawa14 <kurokawa14@gmail.com>
2021-10-31 15:27:39 +0900

    モック生成先修正

--- core/usecase/useruc/user_test.go
+++ core/usecase/useruc/user_test.go
@@ -3,11 +3,11 @@ package useruc
 import (
 	"testing"
 
-	// "github.com/naoyakurokawa/ddd_menta/core/domain/userdm"
+	"github.com/naoyakurokawa/ddd_menta/core/domain/userdm"
 	// "github.com/naoyakurokawa/ddd_menta/core/infrastructure/repoimpl"
 	"github.com/golang/mock/gomock"
 
-	mock "github.com/naoyakurokawa/ddd_menta/core/usecase/useruc/mock_useruc"
+	mock "github.com/naoyakurokawa/ddd_menta/core/domain/userdm/mock_userdm"
 	// "time"
 )
 
@@ -18,13 +18,25 @@ func TestCreate(t *testing.T) {
 		password = "abcd12341231"
 		profile  = "プログラマーです"
 	)
+	userID, err := userdm.NewUserID()
+	if err != nil {
+		t.Errorf("failed to NewUserID: %v", err)
+	}
+	emailIns, err := userdm.NewEmail(email)
+	if err != nil {
+		t.Errorf("failed to NewEmail: %v", err)
+	}
+	user, err := userdm.NewUser(userID, name, emailIns, password, profile)
+	if err != nil {
+		t.Errorf("failed to NewUser: %v", err)
+	}
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	mockUserUsecase := mock.NewMockUserUsecase(ctrl)
+	mockUserUsecase := mock.NewMockUserRepository(ctrl)
 
-	mockUserUsecase.EXPECT().Create(name, email, password, profile)
-	_, err := mockUserUsecase.Create(name, email, password, profile)
+	mockUserUsecase.EXPECT().Create(user)
+	_, err = mockUserUsecase.Create(user)
 
 	if err != nil {
 		t.Errorf("failed to userRepository.Create: %v", err)
