33a318e41f73ea2311dbf46d422ac25c7011a8fb
kurokawa14 <kurokawa14@gmail.com>
2022-01-25 22:15:16 +0900

    レビュー対応

--- core/infrastructure/db/mysql.go
+++ core/infrastructure/db/mysql.go
@@ -10,31 +10,16 @@ import (
 var (
 	once sync.Once
 	db   *gorm.DB
-	err  error
 )
 
-// NewDB DBと接続する
 func NewDB() *gorm.DB {
 	//todo: 環境変数化
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	fmt.Printf("読み込み出来ませんでした: %v", err)
-	// }
-	// USER := os.Getenv("DB_USER")
-	// PASS := os.Getenv("DB_PASS")
-	// PROTOCOL := "tcp(" + os.Getenv("DB_ADDRESS") + ")"
-	// DB_NAME := os.Getenv("DB_NAME")
-	// CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DB_NAME
-	// db, err := gorm.Open("mysql", CONNECT)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
 	once.Do(func() {
-		db, err = gorm.Open("mysql", "ddd_menta:ddd_menta@tcp(localhost)/ddd_menta?parseTime=true")
+		conn, err := gorm.Open("mysql", "ddd_menta:ddd_menta@tcp(localhost)/ddd_menta?parseTime=true")
 		if err != nil {
 			panic(err)
 		}
+		db = conn
 	})
 
 	return db
