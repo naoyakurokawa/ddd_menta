f3b3ffb1c46a08c9b78630a41577240f53d965c1
naoyakurokawa <kurokawa14@gmail.com>
2022-02-23 14:45:08 +0900

    テスト追加

---
+++ core/usecase/contractuc/contractuc_test.go
@@ -0,0 +1,151 @@
+package contractuc
+
+import (
+	"time"
+
+	"github.com/naoyakurokawa/ddd_menta/core/domain/contractdm"
+	"github.com/naoyakurokawa/ddd_menta/core/domain/mentordm"
+	"github.com/naoyakurokawa/ddd_menta/core/domain/userdm"
+	"golang.org/x/xerrors"
+)
+
+type userParams struct {
+	userID    userdm.UserID
+	name      string
+	email     userdm.Email
+	password  userdm.Password
+	profile   string
+	createdAt time.Time
+}
+
+type mentorParams struct {
+	userID                userdm.UserID
+	mentorID              mentordm.MentorID
+	title                 string
+	mainImg               string
+	subImg                string
+	category              string
+	detial                string
+	mentorSkillID         mentordm.MentorSkillID
+	mentorTag             string
+	mentorAssessment      uint16
+	mentorExperienceYears mentordm.ExperienceYears
+	planID                mentordm.PlanID
+	planTitle             string
+	planCategory          string
+	planTag               string
+	planDetial            string
+	planType              mentordm.PlanType
+	planPrice             uint16
+	planActiveStatus      mentordm.PlanStatus
+	planBusyStatus        mentordm.PlanStatus
+	createdAt             time.Time
+}
+
+type contractParams struct {
+	contractID contractdm.ContractID
+	userID     userdm.UserID
+	mentorID   mentordm.MentorID
+	planID     mentordm.PlanID
+	status     contractdm.Status
+	createdAt  time.Time
+	updatedAt  time.Time
+}
+
+var (
+	up          userParams
+	mp          mentorParams
+	cp          contractParams
+	userCareers []userdm.UserCareer
+	userSkills  []userdm.UserSkill
+)
+
+func setupUser() error {
+	//ユーザー
+	userID := userdm.NewUserID()
+	email, err := userdm.NewEmail("test@gmail.com")
+	if err != nil {
+		return xerrors.New("error NewEmail")
+	}
+	password, err := userdm.NewPassword("test12345678")
+	if err != nil {
+		return xerrors.New("error NewPassword")
+	}
+	up = userParams{
+		userID,
+		"テストユーザー",
+		email,
+		password,
+		"テストユーザーです",
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local),
+	}
+	return nil
+}
+
+func setupMentor() error {
+	mentorID := mentordm.NewMentorID()
+	mentorSkillID := mentordm.NewMentorSkillID()
+	planID := mentordm.NewPlanID()
+	experienceYears, err := mentordm.NewExperienceYears(uint16(5))
+	if err != nil {
+		return xerrors.New("error NewExperienceYears")
+	}
+	planType, err := mentordm.NewPlanType(uint16(2))
+	if err != nil {
+		return xerrors.New("error NewPlanType")
+	}
+	activePlanStatus, err := mentordm.NewPlanStatus(uint16(1))
+	if err != nil {
+		return xerrors.New("error NewPlanStatus")
+	}
+	busyPlanStatus, err := mentordm.NewPlanStatus(uint16(2))
+	if err != nil {
+		return xerrors.New("error NewPlanStatus")
+	}
+
+	mp = mentorParams{
+		up.userID,
+		mentorID,
+		"プログラミング全般のメンタリング",
+		"/main.jpg",
+		"/sub.jpg",
+		"プログライミング",
+		"設計・開発・テストの一覧をサポートできます",
+		mentorSkillID,
+		"Golang",
+		uint16(5),
+		experienceYears,
+		planID,
+		"DDDのメンタリング",
+		"設計",
+		"DDD",
+		"DDDの設計手法を学べます",
+		planType,
+		uint16(1000),
+		activePlanStatus,
+		busyPlanStatus,
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local),
+	}
+	return nil
+}
+
+func setupContract() error {
+	contractID := contractdm.NewContractID()
+	userID := userdm.NewUserID()
+	mentorID := mentordm.NewMentorID()
+	planID := mentordm.NewPlanID()
+	status, err := contractdm.NewStatus(uint16(1))
+	if err != nil {
+		return xerrors.New("error NewStatus")
+	}
+	cp = contractParams{
+		contractID,
+		userID,
+		mentorID,
+		planID,
+		status,
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local),
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local),
+	}
+	return nil
+}
--- core/usecase/contractuc/create_contract_usecase_test.go
+++ core/usecase/contractuc/create_contract_usecase_test.go
@@ -1,1 +1,92 @@
 package contractuc
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	mock "github.com/naoyakurokawa/ddd_menta/core/domain/contractdm/mock_contractdm"
+	"github.com/naoyakurokawa/ddd_menta/core/domain/mentordm"
+	mockMentor "github.com/naoyakurokawa/ddd_menta/core/domain/mentordm/mock_mentordm"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/xerrors"
+)
+
+func TestCreate(t *testing.T) {
+	asserts := assert.New(t)
+	for _, td := range []struct {
+		title string
+		input uint16
+		err   error
+	}{
+		{
+			title: "PlanStatusがActiveのとき_エラーが発生しないこと",
+			input: uint16(1),
+			err:   nil,
+		},
+		{
+			title: "PlanStatusがBusyのとき_エラーが発生すること",
+			input: uint16(2),
+			err:   xerrors.New("This plan is not active"),
+		},
+	} {
+		t.Run(td.title, func(t *testing.T) {
+			var (
+				mentorSkills []mentordm.MentorSkill
+				plans        []mentordm.Plan
+			)
+			setupUser()
+			setupMentor()
+			setupContract()
+
+			ctrl := gomock.NewController(t)
+			mockContractRepository := mock.NewMockContractRepository(ctrl)
+			mockContractRepository.EXPECT().Create(gomock.Any()).Return(nil)
+			mockMentorRepository := mockMentor.NewMockMentorRepository(ctrl)
+			mentorSkill, err := mentordm.NewMentorSkill(
+				mp.mentorSkillID,
+				mp.mentorTag,
+				mp.mentorAssessment,
+				mp.mentorExperienceYears,
+			)
+			require.NoError(t, err)
+			mentorSkills = append(mentorSkills, *mentorSkill)
+			planStatus, err := mentordm.NewPlanStatus(td.input)
+			plan, err := mentordm.NewPlan(
+				mp.planID,
+				mp.planTitle,
+				mp.category,
+				mp.planTag,
+				mp.planDetial,
+				mp.planType,
+				mp.planPrice,
+				planStatus,
+			)
+			require.NoError(t, err)
+			plans = append(plans, *plan)
+			mentor, err := mentordm.NewMentor(
+				mp.mentorID,
+				mp.userID,
+				mp.title,
+				mp.mainImg,
+				mp.subImg,
+				mp.category,
+				mp.detial,
+				mentorSkills,
+				plans,
+			)
+			require.NoError(t, err)
+			mockMentorRepository.EXPECT().FindByID(gomock.Any()).Return(mentor, nil)
+			contractUsecase := NewCreateContractUsecase(mockContractRepository, mockMentorRepository)
+			err = contractUsecase.Create(
+				string(up.userID),
+				string(mp.mentorID),
+				string(mp.planID),
+			)
+			if err == nil {
+				asserts.Equal(err, td.err)
+			}
+		})
+	}
+
+}
