ca68655e077cabd6406ae5ede250fb70e962ce35
naoyakurokawa <kurokawa14@gmail.com>
2022-03-09 21:37:54 +0900

    テスト追加

--- core/domain/recruitdm/recruit_test.go
+++ core/domain/recruitdm/recruit_test.go
@@ -70,3 +70,66 @@ func TestNewRecruit(t *testing.T) {
 		})
 	}
 }
+
+func TestReconstruct(t *testing.T) {
+	for _, td := range []struct {
+		title       string
+		inputTitle  string
+		budget      uint16
+		detail      string
+		expectedErr error
+	}{
+		{
+			title:       "titleが空の時_エラーが発生すること",
+			inputTitle:  "",
+			budget:      rp.budget,
+			detail:      rp.detail,
+			expectedErr: errors.New("title must not be empty"),
+		},
+		{
+			title:       "titleが255文字を超える場合_エラーが発生すること",
+			inputTitle:  strings.Repeat("a", 256),
+			budget:      rp.budget,
+			detail:      rp.detail,
+			expectedErr: errors.New("title must less than 255"),
+		},
+		{
+			title:       "budgetが1000円未満の場合_エラーが発生すること",
+			inputTitle:  rp.title,
+			budget:      999,
+			detail:      rp.detail,
+			expectedErr: errors.New("budget more than ¥1000"),
+		},
+		{
+			title:       "detailが空の時_エラーが発生すること",
+			inputTitle:  rp.title,
+			budget:      rp.budget,
+			detail:      "",
+			expectedErr: errors.New("title must not be empty"),
+		},
+		{
+			title:       "detailが2000文字を超える場合_エラーが発生すること",
+			inputTitle:  rp.title,
+			budget:      rp.budget,
+			detail:      strings.Repeat("a", 2001),
+			expectedErr: errors.New("title must less than 2000"),
+		},
+	} {
+		t.Run(td.title, func(t *testing.T) {
+			_, err := Reconstruct(
+				rp.recruitID.String(),
+				rp.userID.String(),
+				td.inputTitle,
+				td.budget,
+				rp.recruitTypeOnce.Uint16(),
+				td.detail,
+				rp.recruitStatusDraft.Uint16(),
+			)
+			if td.expectedErr != nil {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+		})
+	}
+}
