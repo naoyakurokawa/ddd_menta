3086eb73c95e4e3cf6cd6023850972a40806c51f
naoyakurokawa <kurokawa14@gmail.com>
2022-03-11 21:38:17 +0900

    レビュー対応

--- core/domain/recruitdm/recruit_status_vo_test.go
+++ core/domain/recruitdm/recruit_status_vo_test.go
@@ -39,32 +39,44 @@ func TestNewRecruitStatus(t *testing.T) {
 func TestStringRecruitStatus(t *testing.T) {
 	asserts := assert.New(t)
 	for _, td := range []struct {
-		title    string
-		input    uint16
-		expected string
+		title       string
+		input       uint16
+		expected    string
+		expectedErr error
 	}{
 		{
-			title:    "RecruitStatusが1のとき_「下書き」がレスポンスされること",
-			input:    uint16(1),
-			expected: "下書き",
+			title:       "RecruitStatusが1のとき_「下書き」がレスポンスされること",
+			input:       uint16(1),
+			expected:    "下書き",
+			expectedErr: nil,
 		},
 		{
-			title:    "RecruitStatusが2のとき_「公開」がレスポンスされること",
-			input:    uint16(2),
-			expected: "公開",
+			title:       "RecruitStatusが2のとき_「公開」がレスポンスされること",
+			input:       uint16(2),
+			expected:    "公開",
+			expectedErr: nil,
 		},
 		{
-			title:    "RecruitStatusが3のとき_「募集終了」がレスポンスされること",
-			input:    uint16(3),
-			expected: "募集終了",
+			title:       "RecruitStatusが3のとき_「募集終了」がレスポンスされること",
+			input:       uint16(3),
+			expected:    "募集終了",
+			expectedErr: nil,
+		},
+		{
+			title:       "RecruitStatusが0のとき_エラーが発生することこと",
+			input:       uint16(0),
+			expectedErr: errors.New("RecruitStatus must be 1 or 2 or 3"),
 		},
 	} {
 		t.Run(td.title, func(t *testing.T) {
 			recruitStatus, err := NewRecruitStatus(td.input)
-			require.NoError(t, err)
-			actual := recruitStatus.String()
-
-			asserts.Equal(actual, td.expected)
+			if td.expectedErr != nil {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				actual := recruitStatus.String()
+				asserts.Equal(actual, td.expected)
+			}
 		})
 	}
 }
