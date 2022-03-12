1b863ade37d9e99776e263bac927d3dd221df731
naoyakurokawa <kurokawa14@gmail.com>
2022-03-12 23:41:46 +0900

    テスト追加

---
+++ core/usecase/recruituc/recruituc_test.go
@@ -0,0 +1,57 @@
+package recruituc
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/naoyakurokawa/ddd_menta/core/domain/recruitdm"
+	"github.com/naoyakurokawa/ddd_menta/core/domain/userdm"
+)
+
+type recruitParams struct {
+	recruitID               recruitdm.RecruitID
+	userID                  userdm.UserID
+	title                   string
+	budget                  uint32
+	recruitTypeOnce         recruitdm.RecruitType
+	recruitTypeSubscription recruitdm.RecruitType
+	detail                  string
+	recruitStatusDraft      recruitdm.RecruitStatus
+	recruitStatusPublished  recruitdm.RecruitStatus
+	recruitStatusTerminated recruitdm.RecruitStatus
+	createdAt               time.Time
+	updatedAt               time.Time
+}
+
+var (
+	rp recruitParams
+)
+
+func TestMain(m *testing.M) {
+	if err := setup(); err != nil {
+		fmt.Printf("%+v", err)
+		return
+	}
+	m.Run()
+}
+
+func setup() error {
+	recruitID := recruitdm.NewRecruitID()
+	userID := userdm.NewUserID()
+	rp = recruitParams{
+		recruitID,
+		userID,
+		"DDDの基礎を教えて下さい",
+		5000,
+		recruitdm.Once,
+		recruitdm.Subscription,
+		"DDDによる開発をサポートしてもらいたく募集しました",
+		recruitdm.Draft,
+		recruitdm.Published,
+		recruitdm.Terminated,
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local),
+		time.Date(2000, 1, 1, 0, 0, 0, 0, time.Local),
+	}
+	return nil
+}
