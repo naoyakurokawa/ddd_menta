58c614db6be30acf72e41a5ca1f1032471413213
naoyakurokawa <kurokawa14@gmail.com>
2022-03-19 14:18:08 +0900

    テスト修正

--- core/domain/suggestiondm/suggestion_test.go
+++ core/domain/suggestiondm/suggestion_test.go
@@ -69,9 +69,7 @@ func TestNewSuggestion(t *testing.T) {
 }
 
 func TestReconstruct(t *testing.T) {
-	asserts := assert.New(t)
-	for _, td := range []struct {
-		title            string
+	type suggestionFields struct {
 		suggestionID     string
 		mentorID         string
 		recruitID        string
@@ -79,117 +77,109 @@ func TestReconstruct(t *testing.T) {
 		suggestionType   uint16
 		detail           string
 		suggestionStatus uint16
-		expectedErr      error
+	}
+
+	asserts := assert.New(t)
+	for _, td := range []struct {
+		title             string
+		prepareSuggestion func(s *suggestionFields) error
+		expectedErr       error
 	}{
 		{
-			title:            "priceが1000円未満の場合_エラーが発生すること",
-			suggestionID:     sp.suggestionID.String(),
-			recruitID:        sp.recruitID.String(),
-			mentorID:         sp.mentorID.String(),
-			price:            999,
-			suggestionType:   sp.suggestionTypeOnce.Uint16(),
-			detail:           sp.detail,
-			suggestionStatus: sp.suggestionStatusApproval.Uint16(),
-			expectedErr:      errors.New("price more than ¥1000"),
+			title: "priceが1000円未満の場合_エラーが発生すること",
+			prepareSuggestion: func(s *suggestionFields) error {
+				s.price = 999
+				return nil
+			},
+			expectedErr: errors.New("price more than ¥1000"),
 		},
 		{
-			title:            "detailが空の時_エラーが発生すること",
-			suggestionID:     sp.suggestionID.String(),
-			recruitID:        sp.recruitID.String(),
-			mentorID:         sp.mentorID.String(),
-			price:            sp.price,
-			suggestionType:   sp.suggestionTypeOnce.Uint16(),
-			detail:           "",
-			suggestionStatus: sp.suggestionStatusApproval.Uint16(),
-			expectedErr:      errors.New("price more than ¥1000"),
+			title: "detailが空の時_エラーが発生すること",
+			prepareSuggestion: func(s *suggestionFields) error {
+				s.detail = ""
+				return nil
+			},
+			expectedErr: errors.New("price more than ¥1000"),
 		},
 		{
-			title:            "detailが2000文字を超える場合_エラーが発生すること",
-			suggestionID:     sp.suggestionID.String(),
-			recruitID:        sp.recruitID.String(),
-			mentorID:         sp.mentorID.String(),
-			price:            sp.price,
-			suggestionType:   sp.suggestionTypeOnce.Uint16(),
-			detail:           strings.Repeat("a", 2001),
-			suggestionStatus: sp.suggestionStatusApproval.Uint16(),
-			expectedErr:      errors.New("detail must less than 2000"),
+			title: "detailが2000文字を超える場合_エラーが発生すること",
+			prepareSuggestion: func(s *suggestionFields) error {
+				s.detail = strings.Repeat("a", 2001)
+				return nil
+			},
+			expectedErr: errors.New("detail must less than 2000"),
 		},
 		{
-			title:            "suggestionIDが空の場合_エラーが発生すること",
-			suggestionID:     "",
-			recruitID:        sp.recruitID.String(),
-			mentorID:         sp.mentorID.String(),
-			price:            sp.price,
-			suggestionType:   sp.suggestionTypeOnce.Uint16(),
-			detail:           sp.detail,
-			suggestionStatus: sp.suggestionStatusApproval.Uint16(),
-			expectedErr:      errors.New("error NewRecruitIDByVal"),
+			title: "suggestionIDが空の場合_エラーが発生すること",
+			prepareSuggestion: func(s *suggestionFields) error {
+				s.suggestionID = ""
+				return nil
+			},
+			expectedErr: errors.New("error NewRecruitIDByVal"),
 		},
 		{
-			title:            "recruitIDが空の場合_エラーが発生すること",
-			suggestionID:     sp.suggestionID.String(),
-			recruitID:        "",
-			mentorID:         sp.mentorID.String(),
-			price:            sp.price,
-			suggestionType:   sp.suggestionTypeOnce.Uint16(),
-			detail:           sp.detail,
-			suggestionStatus: sp.suggestionStatusApproval.Uint16(),
-			expectedErr:      errors.New("error NewRecruitIDByVal"),
+			title: "recruitIDが空の場合_エラーが発生すること",
+			prepareSuggestion: func(s *suggestionFields) error {
+				s.recruitID = ""
+				return nil
+			},
+			expectedErr: errors.New("error NewRecruitIDByVal"),
 		},
 		{
-			title:            "mentorIDが空の場合_エラーが発生すること",
-			suggestionID:     sp.suggestionID.String(),
-			recruitID:        sp.recruitID.String(),
-			mentorID:         "",
-			price:            sp.price,
-			suggestionType:   sp.suggestionTypeOnce.Uint16(),
-			detail:           sp.detail,
-			suggestionStatus: sp.suggestionStatusApproval.Uint16(),
-			expectedErr:      errors.New("error NewMentorIDByVal"),
+			title: "mentorIDが空の場合_エラーが発生すること",
+			prepareSuggestion: func(s *suggestionFields) error {
+				s.mentorID = ""
+				return nil
+			},
+			expectedErr: errors.New("error NewMentorIDByVal"),
 		},
 		{
-			title:            "suggestionTypeが0の場合_エラーが発生すること",
-			suggestionID:     sp.suggestionID.String(),
-			recruitID:        sp.recruitID.String(),
-			mentorID:         sp.mentorID.String(),
-			price:            sp.price,
-			suggestionType:   0,
-			detail:           sp.detail,
-			suggestionStatus: sp.suggestionStatusApproval.Uint16(),
-			expectedErr:      errors.New("error NewSuggestionType"),
+			title: "suggestionTypeが0の場合_エラーが発生すること",
+			prepareSuggestion: func(s *suggestionFields) error {
+				s.suggestionType = 0
+				return nil
+			},
+			expectedErr: errors.New("error NewSuggestionType"),
 		},
 		{
-			title:            "suggestionStatusが0の場合_エラーが発生すること",
-			suggestionID:     sp.suggestionID.String(),
-			recruitID:        sp.recruitID.String(),
-			mentorID:         sp.mentorID.String(),
-			price:            sp.price,
-			suggestionType:   sp.suggestionTypeOnce.Uint16(),
-			detail:           sp.detail,
-			suggestionStatus: 0,
-			expectedErr:      errors.New("error NewSuggestionStatus"),
+			title: "suggestionStatusが0の場合_エラーが発生すること",
+			prepareSuggestion: func(s *suggestionFields) error {
+				s.suggestionStatus = 0
+				return nil
+			},
+			expectedErr: errors.New("error NewSuggestionStatus"),
 		},
 		{
-			title:            "想定通りのSuggestionオブジェクトが生成されること",
-			suggestionID:     sp.suggestionID.String(),
-			recruitID:        sp.recruitID.String(),
-			mentorID:         sp.mentorID.String(),
-			price:            sp.price,
-			suggestionType:   sp.suggestionTypeOnce.Uint16(),
-			detail:           sp.detail,
-			suggestionStatus: sp.suggestionStatusApproval.Uint16(),
-			expectedErr:      nil,
+			title:             "想定通りのSuggestionオブジェクトが生成されること",
+			prepareSuggestion: nil,
+			expectedErr:       nil,
 		},
 	} {
 		t.Run(td.title, func(t *testing.T) {
+			s := suggestionFields{
+				suggestionID:     sp.suggestionID.String(),
+				mentorID:         sp.mentorID.String(),
+				recruitID:        sp.recruitID.String(),
+				price:            sp.price,
+				suggestionType:   sp.suggestionTypeOnce.Uint16(),
+				detail:           sp.detail,
+				suggestionStatus: sp.suggestionStatusApproval.Uint16(),
+			}
+
+			if td.prepareSuggestion != nil {
+				if err := td.prepareSuggestion(&s); err != nil {
+					t.Fatalf("prepareSuggestion() error = %v", err)
+				}
+			}
+
 			suggestion, err := Reconstruct(
-				td.suggestionID,
-				td.mentorID,
-				td.recruitID,
-				td.price,
-				td.suggestionType,
-				td.detail,
-				td.suggestionStatus,
+				s.suggestionID,
+				s.mentorID,
+				s.recruitID,
+				s.price,
+				s.suggestionType,
+				s.detail,
+				s.suggestionStatus,
 			)
 			if td.expectedErr != nil {
 				require.Error(t, err)
