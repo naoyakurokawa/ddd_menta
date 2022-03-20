5ec1ea247b2ae464e4a62e80b184bb3fe9dba8f2
naoyakurokawa <kurokawa14@gmail.com>
2022-03-20 11:45:10 +0900

    微修正

--- core/domain/suggestiondm/suggestion_repository.go
+++ core/domain/suggestiondm/suggestion_repository.go
@@ -3,5 +3,4 @@ package suggestiondm
 type SuggestionRepository interface {
 	Create(suggestion *Suggestion) error
 	FindByID(suggestionID SuggestionID) (*Suggestion, error)
-	// UpdateContractStatus(recruitID RecruitID, contractStatus RecruitStatus) error
 }
